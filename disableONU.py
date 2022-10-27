1b798aa5c2d4371f80d9ddd805fb232b7a713b78
Jacob Larsen <jacobl@hoodriver.coop>
2022-10-27 10:56:25 -0700

    Fixed not setting VLAN and added error check

--- disableONU.py
+++ disableONU.py
@@ -11,8 +11,12 @@ def getONUSettings(baseurl, token, serialNum):
     return json.loads(response.content)
 
 def disableONU(baseurl, token, serialNum, settings, newVLAN):
-    settings['bridgeMode']['ports'][0]['nativeVLAN'] = newVLAN
+    settings['bridgeMode']['ports'][0]['nativeVLAN'] = int(newVLAN)
     url = f"{baseurl}gpon/onus/{serialNum}/settings"
     HEADER = {'x-auth-token': token}
 
     response = requests.put(verify=False, url=url, headers=HEADER, data=str(settings))
+    if response.status_code == 200:
+        print("Success!")
+    else:
+        print("Recieved HTTP Code {}".format(response.status_code))
